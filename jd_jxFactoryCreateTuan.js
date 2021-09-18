3b9f00aca61f94b53375a2c7498e10368081ad6c
accors <61780738+accors@users.noreply.github.com>
2021-09-18 12:32:21 +0800

    Update jd_jxFactoryCreateTuan.js

--- jd_jxFactoryCreateTuan.js
+++ jd_jxFactoryCreateTuan.js
@@ -2,7 +2,7 @@
 京东京喜工厂自动开团
  */
 const $ = new Env('京东京喜工厂自动开团');
-const tuanActiveId = `Rjp4SYMY2Mk2oekYy2aBSg==`;
+const tuanActiveId = `tCo0qqjNLYX_v3sjUL8RcQ==`;
 const JD_API_HOST = 'https://m.jingxi.com';
 const fs = require('fs');
 const notify = $.isNode() ? require('./sendNotify') : '';
